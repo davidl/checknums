54aec57d16b3f4c6ee714a74a569642468d01ad4
David Lantner <davidlantner@gmail.com>
2017-03-29 22:13:47 -0400

    remove duplicates from service response; minor styling changes

--- sw.js
+++ sw.js
@@ -113,7 +113,7 @@
 //});
 
 // Version of the offline cache (change this value everytime you want to update cache)
-var CACHE_NAME = 'version_04'              
+var CACHE_NAME = 'version_005'              
 
 // Add a path you want to cache in this list.
 var URLS = [                
