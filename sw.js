cdf4a98fc126ad71a209133a0a1eeca1a2f287ca
David Lantner <davidlantner@gmail.com>
2018-04-29 11:49:37 -0400

    Replace service worker with code from Simple Offline Site.

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
-'use strict';
+"use strict";
 
-// Version of the offline cache (change this value everytime you want to update cache)
-var CACHE_NAME = 'version_003453'
+console.log('WORKER: executing.');
 
-// Add a path you want to cache in this list.
-var URLS = [                
-  '/',
+/* From Chris Coyier's Simple-Offline-Site:
+   https://github.com/chriscoyier/Simple-Offline-Site/blob/master/js/service-worker.js
+*/
+var version = 'version_003454';
+var offlineFundamentals = [
+  '',
   '/style.css',
   '/main.js',
   'https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js',
@@ -19,48 +21,82 @@ var URLS = [
   'https://fonts.googleapis.com/css?family=Roboto:400,500',
   'https://fonts.gstatic.com/s/roboto/v16/CrYjSnGjrRCn0pd9VQsnFOvvDin1pK8aKteLpeZ5c0A.woff',
   'https://fonts.gstatic.com/s/roboto/v16/RxZJdnzeo3R5zSexge8UUbO3LdcAZYWl9Si6vvxL-qU.woff'
-]
+];
 
-// Respond with cached resources
-// This is called everytime the browser requests resources from the server
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) {
-        // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {
-        // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
-    })
-  )
-})
+self.addEventListener("install", function(event) {
+  console.log('WORKER: install event in progress.');
+  event.waitUntil(
+    caches
+      .open(version + 'fundamentals')
+      .then(function(cache) {
+        return cache.addAll(offlineFundamentals);
+      })
+      .then(function() {
+        console.log('WORKER: install completed');
+      })
+  );
+});
 
-// Cache resources
-self.addEventListener('install', function (e) {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      // cache everything listed on URLS list 
-      return cache.addAll(URLS)
-    })
-  )
-})
-
-// Delete outdated caches
-self.addEventListener('activate', function (e) {
-  e.waitUntil(
-    caches.keys().then(function (keyList) {
-      return Promise.all(keyList.map(function (key, i) {
-        if (keyList[i] !== CACHE_NAME) {
-          console.log('deleting cache : ' + keyList[i] )
-          return caches.delete(keyList[i])
+self.addEventListener("fetch", function(event) {
+  console.log('WORKER: fetch event in progress.');
+  if (event.request.method !== 'GET') {
+    console.log('WORKER: fetch event ignored.', event.request.method, event.request.url);
+    return;
+  }
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then(function(cached) {
+        var networked = fetch(event.request)
+          .then(fetchedFromNetwork, unableToResolve)
+          .catch(unableToResolve);
+        console.log('WORKER: fetch event', cached ? '(cached)' : '(network)', event.request.url);
+        return cached || networked;
+        function fetchedFromNetwork(response) {
+          var cacheCopy = response.clone();
+          console.log('WORKER: fetch response from network.', event.request.url);
+          caches
+            .open(version + 'pages')
+            .then(function add(cache) {
+              return cache.put(event.request, cacheCopy);
+            })
+            .then(function() {
+              console.log('WORKER: fetch response stored in cache.', event.request.url);
+            });
+            return response;
         }
-      }))
-    })
-  )
+        function unableToResolve () {
+          console.log('WORKER: fetch request failed in both cache and network.');
+          return new Response('<h1>Service Unavailable</h1>', {
+            status: 503,
+            statusText: 'Service Unavailable',
+            headers: new Headers({
+              'Content-Type': 'text/html'
+            })
+          });
+        }
+      })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  console.log('WORKER: activate event in progress.');
+  event.waitUntil(
+    caches
+      .keys()
+      .then(function (keys) {
+        return Promise.all(
+          keys
+            .filter(function (key) {
+              return !key.startsWith(version);
+            })
+            .map(function (key) {
+              return caches.delete(key);
+            })
+        );
+      })
+      .then(function() {
+        console.log('WORKER: activate completed.');
+      })
+  );
 });
